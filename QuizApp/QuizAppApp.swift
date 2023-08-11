61c1fecdc25848a0c0038b2181c7f7eb364f85cb
Rahini <rahink2008@outlook.com>
2023-08-11 12:58:33 -0400

    Initial Commit

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by scholar on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
